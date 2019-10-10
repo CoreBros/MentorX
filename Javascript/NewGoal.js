fb4a36cc60eb0b0dc944490388cc809994ebf6a5
Breezy <jsbreon@gmail.com>
2019-10-09 17:42:40 -0700

    fixed javascript issues with pop ups and further work on layout of 2nd modal for new custom goal

--- Javascript/NewGoal.js
+++ Javascript/NewGoal.js
@@ -2,3 +2,7 @@ function newCustomGoal() {
 	document.getElementById("id01").style.display="none";
 	document.getElementById("id02").style.display="block";
 }
+
+function addCustomGoal() {
+	document.getElementById("id02").style.display="none";
+}
