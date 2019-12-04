f0bb5f88de3d46de3f9256e098d9660b4fae0234
Breezy <jsbreon@gmail.com>
2019-12-04 15:35:58 -0800

    File structuring and merging from live repository

--- Javascript/NewGoal.js
+++
@@ -1,8 +0,0 @@
-function newCustomGoal() {
-	document.getElementById("id01").style.display="none";
-	document.getElementById("id02").style.display="block";
-};
-
-function addCustomGoal() {
-	document.getElementById("id02").style.display="none";
-};
