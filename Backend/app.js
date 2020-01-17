6cf771e5e9a1dd173e9e71edc66617a9b26b9990
Breezy <jsbreon@gmail.com>
2020-01-17 15:53:37 -0800

    Backend files for server side rendering

--- Backend/app.js
+++ Backend/app.js
@@ -19,6 +19,11 @@ app.get("/", function(req,res){
 	res.render("splash");
 });
 
+app.get("/login", function(req,res){
+	res.render("login");
+});
+
+
 
 let port = process.env.PORT;
 	if (port == null || port =="") {
@@ -28,3 +33,5 @@ let port = process.env.PORT;
 app.listen(port, function() {
     console.log("Server started successfully");
 });
+
+
---
+++ Backend/public/js/LogIn.js
@@ -0,0 +1,48 @@
+
+$(function() {
+  $(".btn").click(function() {
+    $(".form-signin").toggleClass("form-signin-left");
+    $(".form-signup").toggleClass("form-signup-left");
+    $(".frame").toggleClass("frame-long");
+    $(".signup-inactive").toggleClass("signup-active");
+    $(".signin-active").toggleClass("signin-inactive");
+    $(".forgot").toggleClass("forgot-left");   
+    $(this).removeClass("idle").addClass("active");
+  });
+});
+
+$(function() {
+  $(".btn-signup").click(function() {
+  $(".nav").toggleClass("nav-up");
+  $(".form-signup-left").toggleClass("form-signup-down");
+  $(".success").toggleClass("success-left"); 
+  $(".frame").toggleClass("frame-short");
+    var timer = setTimeout(function() {
+      window.location='HomePage.html'
+      }, 4000);
+  });
+});
+
+$(function() {
+  $(".btn-signin").click(function() {
+  $(".btn-animate").toggleClass("btn-animate-grow");
+  $(".welcome").toggleClass("welcome-left");
+  $(".cover-photo").toggleClass("cover-photo-down");
+  $(".frame").toggleClass("frame-short");
+  $(".profile-photo").toggleClass("profile-photo-down");
+  $(".btn-goback").toggleClass("btn-goback-up");
+  $(".forgot").toggleClass("forgot-fade");
+  });
+});
+
+
+// When the user clicks anywhere outside of the login form, close it
+$(function(){
+var modal = document.getElementById('id01');
+var loginForm = document.getElementById('id02');
+window.onclick = function(event) {
+  if (event.target == loginForm) {
+    modal.style.display = "none";
+  }
+}
+});
