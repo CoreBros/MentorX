8fcbfbde1573bbb6aef9e6ebc1488adda8133c3a
Breezy <jsbreon@gmail.com>
2020-01-17 15:27:16 -0800

    Added procfile

---
+++ Backend/app.js
@@ -0,0 +1,30 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+mongoose.connect("mongodb+srv://admin-breezy:26inIL0RLvtHC5Ah@cluster0-nr3qq.mongodb.net/mentorxDB", { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.get("/", function(req,res){
+	res.render("splash");
+});
+
+
+let port = process.env.PORT;
+	if (port == null || port =="") {
+		port = 3000;
+	};
+
+app.listen(port, function() {
+    console.log("Server started successfully");
+});
