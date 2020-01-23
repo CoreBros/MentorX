994979a849bb63d60ff90aacb8330b26f721549f
Breezy <jsbreon@gmail.com>
2020-01-22 16:57:53 -0800

    Clean up

--- Backend/public/js/Menu.js
+++
@@ -1,9 +0,0 @@
-// When the user clicks anywhere outside of the login form, close it
-$(function(){
-	var modal = document.getElementById('_dropdownMenu');
-	window.onclick = function(event) {
-  		if (event.target == modal) {
-    	modal.style.display = "none";
-  		}
-	}
-});
